388410b8f4ecd553f161a8f9c37dbf6ea73fd601
Bhairav Valera <bhairavvalera98@gmail.com>
2021-07-25 14:46:48 -0400

    Experience page tweaks

--- src/components/ExperienceCard.js
+++ src/components/ExperienceCard.js
@@ -9,6 +9,8 @@ class ExperienceCard extends Component {
     constructor(props) {
         super(props)
 
+        this.onClick = this.onClick.bind(this);
+
         this.imageAliases = {
             'anl': anl,
             'bnl': bnl,
@@ -18,16 +20,44 @@ class ExperienceCard extends Component {
         };
     }
 
+    onClick() {
+        switch (this.props.companyName) {
+            case "netInspect": {
+                window.location.assign('https://www.net-inspect.com')
+                break;
+            }
+            case "percolata": {
+                window.location.assign('https://www.percolata.com')
+                break;
+            }
+            case "anl": {
+                window.location.assign('https://www.anl.gov')
+                break;
+            }
+            case "illinois": {
+                window.location.assign('https://physics.illinois.edu')
+                break;
+            }
+            case "bnl": {
+                window.location.assign('https://www.bnl.gov/world/')
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+    }
+
     render() {
         return (
-            <div id='ExperienceCard'>
+            <div id='ExperienceCard' onClick={this.onClick}>
                 <div id='Logo'>
                     <img src={this.imageAliases[this.props.companyName]} 
                         alt='companyLogo' id='companyLogo'></img>
                 </div>
-                <div id='companyInfo'>
+                {/* <div id='companyInfo'>
                     {this.props.companyName}
-                </div>
+                </div> */}
             </div>
         )
     }
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -6,9 +6,17 @@ import TimelineConnector from '@material-ui/lab/TimelineConnector';
 import TimelineContent from '@material-ui/lab/TimelineContent';
 import TimelineDot from '@material-ui/lab/TimelineDot';
 import { TimelineOppositeContent } from '@material-ui/lab';
+import { withStyles } from '@material-ui/core/styles'
 import ExperienceCard from './ExperienceCard';
 
 class TimeLine extends Component {
+    StyledTimelineContent = withStyles({
+        root: {
+            position: 'relative',
+            bottom: '2rem'
+        }
+    })(TimelineContent)
+
     render() {
         return (
             <Timeline align="left">
@@ -18,9 +26,9 @@ class TimeLine extends Component {
                     <TimelineDot />
                     <TimelineConnector />
                     </TimelineSeparator>
-                    <TimelineContent>
+                    <this.StyledTimelineContent>
                         <ExperienceCard companyName='netInspect'></ExperienceCard>
-                    </TimelineContent>
+                    </this.StyledTimelineContent>
                 </TimelineItem>
                 <TimelineItem>
                     <TimelineOppositeContent>July 2020 — March 2021</TimelineOppositeContent>                    
@@ -28,7 +36,9 @@ class TimeLine extends Component {
                     <TimelineDot/>
                     <TimelineConnector />
                     </TimelineSeparator>
-                    <TimelineContent>Percolata</TimelineContent>
+                    <this.StyledTimelineContent>
+                        <ExperienceCard companyName='percolata'></ExperienceCard>
+                    </this.StyledTimelineContent>
                 </TimelineItem>
                 <TimelineItem>
                     <TimelineOppositeContent>June 2019 — August 2019</TimelineOppositeContent>
@@ -36,7 +46,9 @@ class TimeLine extends Component {
                     <TimelineDot/>
                     <TimelineConnector />
                     </TimelineSeparator>
-                    <TimelineContent>Argonne</TimelineContent>
+                    <this.StyledTimelineContent>
+                        <ExperienceCard companyName='anl'></ExperienceCard>
+                    </this.StyledTimelineContent>
                 </TimelineItem>
                 <TimelineItem>
                     <TimelineOppositeContent>November 2018 — July 2020</TimelineOppositeContent>
@@ -44,14 +56,18 @@ class TimeLine extends Component {
                     <TimelineDot />
                     <TimelineConnector />
                     </TimelineSeparator>
-                    <TimelineContent>Dept. of Physics</TimelineContent>
+                    <this.StyledTimelineContent>
+                        <ExperienceCard companyName='illinois'></ExperienceCard>
+                    </this.StyledTimelineContent>
                 </TimelineItem>
                 <TimelineItem>
                     <TimelineOppositeContent>June 2018 — August 2018</TimelineOppositeContent>
                     <TimelineSeparator>
                     <TimelineDot />
                     </TimelineSeparator>
-                    <TimelineContent>Brookhaven</TimelineContent>
+                    <this.StyledTimelineContent>
+                        <ExperienceCard companyName='bnl'></ExperienceCard>
+                    </this.StyledTimelineContent>
                 </TimelineItem>
             </Timeline>
         )
